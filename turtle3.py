5361620515d990250e104e00a1be5d2a30ace8ee
minaev-aa <minaev.aa@phystech.edu>
2020-09-24 21:25:39 +0300

    v1.0

--- turtle3.py
+++ turtle3.py
@@ -8,21 +8,27 @@ step = 10
 d = {}
 
 pool = [turtle.Turtle(shape='circle') for i in range(number_of_turtles)]
-i=0;
 for unit in pool:
-    i = i + 1
     unit.penup()
     unit.speed(100)
     unit.goto(randint(-300, 300), randint(-300, 300))
-    unit.rt(randint(-180, 180))
+    angle = randint(0, 3600)
+    unit.rt(angle)
     x=str(unit)
-    d.update({x : {'orientation':'1','num': str(i)}})
+    d.update({x : {'orientation':'1','angle': str(angle)}})
+
 k = 1
 for i in range(steps_of_time_number):
     for unit in pool:
         if k<=len(pool):
+            angle = d[str(unit)]['angle']
+            for test in pool:
+                if unit != test:
+                    if unit.distance(test) < 20:
+
+
             if round(unit.xcor(), 5)>=300 or round(unit.xcor(), 5)<=-300 or round(unit.ycor(), 5)>=300 or round(unit.ycor(), 5)<=-300:
-                    d.update({str(unit) : {'orientation' : str(int(d[str(unit)]['orientation'])*(-1)), 'num' : str(i)}})
+                    d.update({str(unit) : {'orientation' : str(int(d[str(unit)]['orientation'])*(-1)), 'angle' : angle}})
             if d[str(unit)]['orientation'] == '1':
                 unit.forward(step)
             if d[str(unit)]['orientation'] == '-1':
