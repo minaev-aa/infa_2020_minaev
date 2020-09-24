ae093af8071a01521703f435288cf8b4a783d255
minaev-aa <minaev.aa@phystech.edu>
2020-09-24 20:38:32 +0300

    v1.0

--- turtle3.py
+++ turtle3.py
@@ -2,17 +2,31 @@ from random import randint
 import turtle
 
 
-number_of_turtles = 5
+number_of_turtles = 25
 steps_of_time_number = 100
+step = 3
+d = {}
 
-
-pool = [turtle.Turtle(shape='turtle') for i in range(number_of_turtles)]
+pool = [turtle.Turtle(shape='circle') for i in range(number_of_turtles)]
+i=0;
 for unit in pool:
+    i = i + 1
     unit.penup()
-    unit.speed(50)
-    unit.goto(randint(-200, 200), randint(-200, 200))
-
-
+    unit.speed(100)
+    unit.goto(randint(-300, 300), randint(-300, 300))
+    unit.rt(randint(-180, 180))
+    x=str(unit)
+    d.update({x : {'orientation':'1','num': str(i)}})
+k = 1
 for i in range(steps_of_time_number):
     for unit in pool:
-        unit.forward(2)
+        if k<len(pool):
+            if round(unit.xcor(), 5)>=300 or round(unit.xcor(), 5)<=-300 or round(unit.ycor(), 5)>=300 or round(unit.ycor(), 5)<=-300:
+                    d.update({str(unit) : {'orientation':'1','num': str(i)}str(int('1')*(-1))})
+            if d[str(unit)] == '1':
+                unit.forward(step)
+            if d[str(unit)] == '-1':
+                unit.forward(-1*step)
+            k = k + 1
+        else:
+            k = 1
