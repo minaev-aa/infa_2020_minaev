5893012a9ac24f627a164ffcb35f5df944e568b7
minaev-aa <minaev.aa@phystech.edu>
2020-09-30 22:51:28 +0300

    NOT work

--- pygame_lab3.1/pygame1.py
+++ pygame_lab3.1/pygame1.py
@@ -10,7 +10,7 @@ pygame.init()
 
 
 FPS = 30
-screen = pygame.display.set_mode((600, 800))
+screen = pygame.display.set_mode((600, 600))
 screen.fill((255, 255, 255, 255))
 '''
 rect(screen, (189, 183, 107, 255), (0,500,600,300) )
@@ -54,18 +54,39 @@ circle(screen, (0, 0, 0, 255), (170, 170), 5)
 polygon(screen, (0, 0, 0, 255),  [(160,130), (200,150), (190,160), (150,140)])
 polygon(screen, (0, 0, 0, 255),  [(222,156), (260,143), (258,153), (220,166)])
 '''
-def smile(x,y,inc):
+def smile(x = 0,y = 0,inc = 0):
+    circle(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)) , (x + inc, y + inc), 62 + inc)
+    circle(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)) , (x + inc, y + inc), 60 + inc)
+    rect(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), (-30 + x + inc / 2,
+                                                                                    20 + y + 1.3 * inc, 60 + inc, 10 + inc/3) )
+    circle(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)) , (-20 + x + inc/2, -20 + y + inc/2), 10 + inc/6)
+    circle(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)) , (0 + x + inc, 0 + y + inc), 1 + inc/6)
+    circle(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)) , (-20 + x + inc/2, -20 + y + inc/2), 5 + inc/12)
+    circle(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)) , (20 + x + inc*1.5, -20 + y + inc/2), random.randint(6,9) + inc/10)
+    circle(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)) , (20 + x + inc*1.5, -20 + y + inc/2), random.randint(2,4) + inc/10)
+    rect(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), (-33 + x + inc/4,
+                                                                                    -32 + y + inc/3, 26 + inc/2, 5 + inc/6) )
+    rect(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), (10 + x + 1.37*inc,
+                                                                                    -29 + y + inc/2.5, 20 + inc/4, 4 + inc/12) )
+
+
+
+
+
+    '''
     circle(screen, (255, 255, 0, 255) , (50, 300), 40)
     rect(screen, (0, 0, 0, 255), (30, 310, 40, 8) )
+
     circle(screen, (255, 0, 0, 255), (30, 284), 8)
     circle(screen, (0, 0, 0, 255), (30, 284), 2)
-    polygon(screen, (0, 0, 0, 255),  [(30, 270), (42, 275), (39, 279), (28, 274)])
+    #polygon(screen, (0, 0, 0, 255),  [(30, 270), (25, 275), (35, 275), (40, 280)])
     circle(screen, (255, 0, 0, 255), (60, 285), 6)
     circle(screen, (0, 0, 0, 255), (60, 285), 1)
-    polygon(screen, (0, 0, 0, 255),  [(53, 278), (63, 272), (62, 276), (52, 282)])
+    #polygon(screen, (0, 0, 0, 255),  [(53, 278), (63, 272), (62, 276), (52, 282)])
+    rect(screen, (0, 67, 0, 255), (23, 275, 15, 4) )
+    '''
 
-
-
+smile(100, 100, 40)
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
@@ -80,9 +101,6 @@ while True:
             x, y = event.pos
             print(x,y)
             os.system('cls')
-            if (x in range(0,50)) and (y in range(0,50)):
-                print('курсор в регионе')
-                time.sleep(0.5)
 
 while not finished:
     clock.tick(FPS)
