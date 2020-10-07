5f9e1ae8e72b662d10cf1d307b3c4142250253b1
minaev-aa <minaev.aa@phystech.edu>
2020-10-07 21:05:16 +0300

    Add gif

--- pygame_lab3.1/town.py
+++ pygame_lab3.1/town.py
@@ -4,6 +4,7 @@
 
 import pygame
 import random
+from PIL import Image
 
 FPS = 60
 run = True
@@ -11,6 +12,8 @@ clock = pygame.time.Clock()
 width = 600
 height = 800
 amount = 1
+frames = []
+
 
 class GameManager:
     def __init__(self, game_window, array_house, array_car, array_cloud):
@@ -174,11 +177,22 @@ array3 = Cloud(win, 7, [70, 60, -70, 60, 150, -150, 190], [680, 635, 580, 335, 1
 
 manager = GameManager(win, array1, array2, array3)
 
+
+
+
 while run:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            frames[0].save('pygame_lab3.1\pic\moving_ball.gif', format = 'GIF',
+                            append_images = frames[1:],
+                            save_all = True,
+                            duration = 100, loop = 0)
             run = False
     manager.redraw()
     pygame.display.flip()
+    img = pygame.image.tostring(win, "RGBA")
+    new_frame = Image.frombytes("RGBA", (600, 800), img)
+    frames.append(new_frame)
+
 pygame.quit()
